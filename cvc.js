28df4dafd9c0530f16e189aea34f1b4d219c4c8d
alan <alanhenrique@ext.cvc.com.br>
2017-07-20 19:44:10 -0300

    atualizou

--- cvc.js
+++ cvc.js
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var steve = function(){
-=======
- var steve = function(){
->>>>>>> 8154fc65f9b784e79c78245819305de2886c7413
+
+
+
     var drop, curso;
 
     drop = "Bruno Calcagno";
 }
-=======
+
 linha 1
 linha 2
 linha 3
 linha 4
->>>>>>> Enviando cvc.js
+
